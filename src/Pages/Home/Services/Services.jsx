3eb74d9a519ddeaf6b48b7a6145005adfc61ca36
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-11 21:14:56 +0600

    added search

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 import { useEffect } from "react";
 import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
   const [isAsc, setIsAsc] = useState(true);
+  const searchRef = useRef();
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
-    fetch(`http://localhost:5000/services?order=${isAsc ? "des" : "asc"}`)
+    fetch(
+      `http://localhost:5000/services?search=${searchValue}&order=${
+        isAsc ? "des" : "asc"
+      }`
+    )
       .then(res => res.json())
       .then(data => {
         setServices(data);
       })
       .catch(err => console.error(err));
-  }, [isAsc]);
+  }, [isAsc, searchValue]);
+
+  const handleSearch = () => {
+    setSearchValue(searchRef.current.value);
+  };
 
   return (
     <div className="py-5">
@@ -29,14 +40,27 @@ const Services = () => {
       </div>
       <div className="">
         <h2 className="">Services: {services.length}</h2>
-        <div className="py-4">
-          {" "}
+        <div className="py-4 flex flex-wrap gap-8 items-start justify-between">
+          <div className="">
+            <input
+              type="search"
+              name="search"
+              ref={searchRef}
+              placeholder="search now"
+            />
+            <button
+              type="button"
+              className="btn btn-ghost bg-blue-300"
+              onClick={handleSearch}>
+              Search
+            </button>
+          </div>
           <button
             type="button"
             className="btn btn-ghost bg-purple-300"
             onClick={() => setIsAsc(!isAsc)}>
-            Sort by {isAsc ? "descending" : "ascending"}{" "}
-          </button>{" "}
+            Sort by {isAsc ? "descending" : "ascending"}
+          </button>
         </div>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map(service => (
