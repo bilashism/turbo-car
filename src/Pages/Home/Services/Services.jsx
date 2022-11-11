90cc628de132fa935b5b12e4fa1ac26f9d12786e
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-11-11 20:36:06 +0600

    added sorting order

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -4,15 +4,16 @@ import ServiceCard from "./ServiceCard";
 
 const Services = () => {
   const [services, setServices] = useState([]);
+  const [isAsc, setIsAsc] = useState(true);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/services`)
+    fetch(`http://localhost:5000/services?order=${isAsc ? "des" : "asc"}`)
       .then(res => res.json())
       .then(data => {
         setServices(data);
       })
       .catch(err => console.error(err));
-  }, []);
+  }, [isAsc]);
 
   return (
     <div className="py-5">
@@ -28,6 +29,15 @@ const Services = () => {
       </div>
       <div className="">
         <h2 className="">Services: {services.length}</h2>
+        <div className="py-4">
+          {" "}
+          <button
+            type="button"
+            className="btn btn-ghost bg-purple-300"
+            onClick={() => setIsAsc(!isAsc)}>
+            Sort by {isAsc ? "descending" : "ascending"}{" "}
+          </button>{" "}
+        </div>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {services.map(service => (
             <ServiceCard key={service._id} service={service} />
